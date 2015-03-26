db5a88e4c533fc673de272282bbbf9b0e77cbb4d
TacoVox <jonas.kahler@icloud.com>
2015-03-26 14:44:30 +0100

    Added timestamp

--- Trux/app/src/main/java/se/gu/tux/trux/datastructure/Data.java
+++ Trux/app/src/main/java/se/gu/tux/trux/datastructure/Data.java
@@ -6,8 +6,10 @@ import java.io.Serializable;
  * Created by jonas on 3/24/15.
  */
 public abstract class Data <T> implements Serializable {
-	private static final long serialVersionUID = 1L;
-	public abstract T getValue();
+    private static final long serialVersionUID = 1L;
+    private long timestamp;
+    public abstract T getValue();
+    public long getTimeStamp() {return timestamp;};
     public abstract void setValue(T value);
     public abstract boolean isOnServerSide();
 }
