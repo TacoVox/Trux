7ff4c6322b54bf4709a1b98b3c15a629d7192e9c
TacoVox <jonas.kahler@icloud.com>
2015-04-02 12:36:34 +0200

    Added a class which is responsible for inserting Metrics to the database

--- TruxServer/src/main/java/se/gu/tux/truxserver/dbconnect/MetricInserter.java
+++ TruxServer/src/main/java/se/gu/tux/truxserver/dbconnect/MetricInserter.java
@@ -17,6 +17,7 @@ package se.gu.tux.truxserver.dbconnect;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import se.gu.tux.trux.datastructure.MetricData;
 
@@ -25,38 +26,62 @@ import se.gu.tux.truxserver.logger.Logger;
  *
  * @author jonas
  */
-public class MetricHandler {
+public class MetricInserter implements Runnable {
     /**
      * Static part.
      */
-    private static MetricHandler mh;
+    private static MetricInserter mi;
     
     static
     {
-        if(mh ==  null)
-            mh = new MetricHandler();
+        if(mi ==  null)
+            mi = new MetricInserter();
     }
     
-    protected static MetricHandler getInstance()
+    protected static MetricInserter getInstance()
     {
-        return mh;
+        return mi;
     }
     
-    protected static MetricHandler gI()
+    protected static MetricInserter gI()
     {
-        return mh;
+        return mi;
     }
     
     /**
      * Non-static part.
      */
-    private MetricHandler() {}
+    private LinkedBlockingQueue queue;
     
-    public boolean insertMetric(MetricData md)
+    private MetricInserter() {
+        queue = new LinkedBlockingQueue();
+    }
+    
+    @Override
+    public void run() {
+        Logger.gI().addMsg("InsertionHandler is running and waiting for input");
+        
+        while(true) {
+            try {
+                insertMetric((MetricData)queue.take());
+            }
+            catch (Exception e) {
+                Logger.gI().addError(e.getMessage());
+            }
+        }
+        
+    }
+    
+    public synchronized void addToDB(MetricData md) {
+        queue.add(md);
+    }
+    
+    private boolean insertMetric(MetricData md)
     {
+        DBConnector dbc = ConnectionPool.gI().getDBC();
         try
         {   
-            PreparedStatement pst = DBConnector.gS().getConnection().prepareStatement(
+            PreparedStatement pst = dbc.getConnection().prepareStatement(
                     "INSERT INTO metric(value, timestamp, type, userid) " + 
                             "VALUES(?, ?, ?, ?);");
 		
@@ -73,44 +98,10 @@ public class MetricHandler {
         {
             Logger.gI().addError(e.toString());
         }
-
-	return false;
-    }
-    
-    public MetricData getMetric(MetricData md)
-    {
-        try
-	{
-            String selectStmnt = "SELECT AVG(value) AS avg FROM metric WHERE timestamp " +
-                    "BETWEEN ? AND ? AND type = ? AND userid = ?;";
-            
-            Logger.getInstance().addDebug(selectStmnt);
-            
-            PreparedStatement pst = DBConnector.gR().getConnection().prepareStatement(
-                    selectStmnt);
-	    
-	    pst.setLong(1, md.getTimeStamp() - md.getTimeFrame());
-            pst.setLong(2, md.getTimeStamp());
-            pst.setString(3, md.getClass().getSimpleName());
-            pst.setLong(4, 0);
-            
-            System.out.println(pst.getParameterMetaData().toString());
-            
-            Logger.gI().addDebug(pst.toString());
-	    
-	    ResultSet rs = pst.executeQuery(selectStmnt);
-	    
-	    while (rs.next())
-	    {
-		md.setValue(rs.getDouble("avg"));
-		break;
-	    }
-	}
-	catch (Exception e)
-	{
-	    Logger.gI().addError(e.toString());
-	}
+        finally {
+            ConnectionPool.gI().releaseDBC(dbc);
+        }
         
-        return md;
+	return false;
     }
 }
