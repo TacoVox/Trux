985b7d194a008b114d76d8d286a651e2154a53a8
TacoVox <jonas.kahler@icloud.com>
2015-04-02 12:36:58 +0200

    Added a class which is responsible for fetching data from our database

--- TruxServer/src/main/java/se/gu/tux/truxserver/dbconnect/MetricReceiver.java
+++ TruxServer/src/main/java/se/gu/tux/truxserver/dbconnect/MetricReceiver.java
@@ -15,10 +15,79 @@
  */
 package se.gu.tux.truxserver.dbconnect;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import se.gu.tux.trux.datastructure.MetricData;
+import se.gu.tux.truxserver.logger.Logger;
+
 /**
  *
  * @author jonas
  */
-public class RetrieveHandler {
+public class MetricReceiver {
+    /**
+     * Static part.
+     */
+    private static MetricReceiver mr = null;
+    
+    static {
+        if (mr == null)
+            mr = new MetricReceiver();
+    }
+    
+    public static MetricReceiver getInstance() {
+        return mr;
+    }
+    
+    public static MetricReceiver gI() {
+        return mr;
+    }
+    
+    /**
+     * Non-static part.
+     */
+    private MetricReceiver() {}
+    
+    public MetricData getMetric(MetricData md)
+    {
+        DBConnector dbc = ConnectionPool.gI().getDBC();
+        
+        try
+	{
+            String selectStmnt = "SELECT AVG(value) AS avg FROM metric WHERE " +
+                    "type = ? AND userid = ? AND timestamp BETWEEN ? AND ?;";
+            
+            Logger.getInstance().addDebug(selectStmnt);
+            
+            PreparedStatement pst = dbc.getConnection().prepareStatement(
+                    selectStmnt);
+	    
+            pst.setString(1, md.getClass().getSimpleName());
+            pst.setLong(2, 0);
+	    pst.setLong(3, md.getTimeStamp() - md.getTimeFrame());
+            pst.setLong(4, md.getTimeStamp()); 
+            
+            System.out.println(pst.getParameterMetaData().toString());
+            
+            Logger.gI().addDebug(pst.toString());
+	    
+	    ResultSet rs = pst.executeQuery();
+	    
+	    while (rs.next())
+	    {
+		md.setValue(rs.getDouble("avg"));
+		break;
+	    }
+	}
+	catch (Exception e)
+	{
+	    Logger.gI().addError(e.toString());
+	}
+        finally {
+            ConnectionPool.gI().releaseDBC(dbc);
+        }
+        
+        return md;
+    }
     
 }
