d82e3b9725ad5043d6c6b6e4b0c087f1050de785
Ivo Vryashkov <ivo.vryashkov@gmail.com>
2015-04-07 22:38:56 +0200

    some work on IServerConnector, tested and worked fine with receiving and sending data, maybe try and see if we can combine Jerker :)

--- Trux/app/src/main/java/se/gu/tux/trux/technical_services/IServerConnector.java
+++ Trux/app/src/main/java/se/gu/tux/trux/technical_services/IServerConnector.java
@@ -112,11 +112,13 @@ public class IServerConnector
 
         public void stopRun()
         {
-            if (cs != null || !cs.isClosed())
+            if (cs != null || !cs.isClosed() || out != null || in != null)
             {
                 try
                 {
                     cs.close();
+                    in.close();
+                    out.close();
                     isRunning = false;
                 }
                 catch (IOException e) { e.printStackTrace(); }
@@ -128,15 +130,15 @@ public class IServerConnector
 
         public synchronized Data sendQuery(Data data)
         {
+            // if data is null, return
+            if (data == null)   { return null; }
 
-            // check if the out and in streams are open, if not return null
-            // we don't care here if there is a problem in the main thread
-            // we just want to send a query and for that we need the input and
-            // output streams, in case there is a problem with main thread it
-            // should be fixed there, not here!
-            if (out == null || in == null || data == null)
-            { return null; }
+            // check if the out and in streams are open, if not connect
+            if (cs == null || cs.isClosed() || out == null || in == null)
+            { connect(); }
 
+            // send the query, at this point should not be any problems
+            // with the connection or the streams
             try
             {
                 out.writeObject(data);
@@ -145,6 +147,20 @@ public class IServerConnector
             }
             catch (IOException e)
             {
+                // in case something went wrong!
+                // close the socket and streams, this will also invoke
+                // main thread to try and connect
+                try
+                {
+                    cs.close();
+                    in.close();
+                    out.close();
+                }
+                catch (IOException e1)
+                {
+                    e1.printStackTrace();
+                }
+
                 System.out.println("\nIOException in ConnectorRunnable sendQuery()\n");
                 e.printStackTrace();
             }
@@ -159,7 +175,7 @@ public class IServerConnector
         }
 
 
-        private synchronized void reconnect()
+        private synchronized void connect()
         {
 
             while (cs == null || cs.isClosed() || out == null || in == null)
@@ -175,12 +191,12 @@ public class IServerConnector
                 }
                 catch (StreamCorruptedException e)
                 {
-                    System.out.println("\nStreamCorruptException in ConnectorRunnable reconnect()\n");
+                    System.out.println("\nStreamCorruptException in ConnectorRunnable connect()\n");
                     e.printStackTrace();
                 }
                 catch (UnknownHostException e)
                 {
-                    System.out.println("\nUnknownHostException in ConnectorRunnable reconnect()\n");
+                    System.out.println("\nUnknownHostException in ConnectorRunnable connect()\n");
                     e.printStackTrace();
                 }
                 catch (IOException e)
@@ -189,6 +205,18 @@ public class IServerConnector
                     System.err.println("Could not connect to " + serverAddress +
                             ". Retrying in 10s...");
                     e.printStackTrace();
+
+                    // close all channels
+                    try
+                    {
+                        cs.close();
+                        in.close();
+                        out.close();
+                    }
+                    catch (IOException e1)
+                    {
+                        e1.printStackTrace();
+                    }
                 }
 
                 try
@@ -203,7 +231,7 @@ public class IServerConnector
 
             } // end while
 
-        } // end reconnect()
+        } // end connect()
 
 
         @Override
@@ -217,18 +245,23 @@ public class IServerConnector
                 // connection to server
                 if (cs == null || cs.isClosed() || out == null || in == null)
                 {
-                    reconnect();
+                    connect();
                 }
 
-                // we connected
+                // we connected or have already running connection
                 try
                 {
                     Data data = null;
 
-                    // if the queue is not empty, take the next available element
+                    // if the queue is not empty, peek at the next available element
+                    // do not remove!
+                    // later when we are done with the object, we will remove it
+                    // we do that in case transmission is interrupted, if it is
+                    // then we don't want to remove the object, since it is in a queue
+                    // and priority matters, so in that case we simply don't remove it
                     if (!queue.isEmpty())
                     {
-                        data = queue.take();
+                        data = queue.peek();
                     }
 
 
@@ -250,16 +283,23 @@ public class IServerConnector
                             System.out.println("Server connector: Received data " + dataIn.getValue().toString());
                         }
 
+                        // make sure everything is fine with channels
+                        // after reading and writing
+                        // remove object from queue here, last call in method
+                        if (cs != null && out != null && in != null)
+                        {
+                            System.out.println("\nRemoving object from queue in ConnectorRunnable run()\n");
+
+                            queue.poll();
+                        }
+
                     }
 
                 }
-                catch (InterruptedException e)
-                {
-                    System.out.println("\nInterruptedException in ConnectorRunnable run()\n");
-                    e.printStackTrace();
-                }
                 catch (IOException e)
                 {
+                    // in case something went wrong with writing and reading
+                    // to the streams close channels to invoke reconnection
                     try
                     {
                         cs.close();
