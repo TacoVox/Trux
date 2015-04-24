10f149ffdadf8fa70d90191b2e43f7eb548040fa
Dennis <realdkk@gmail.com>
2015-04-24 17:01:54 +0200

    All the detailed windows now show avarage stats, the stats are preloaded in Stats.java

--- Trux/app/src/main/java/se/gu/tux/trux/gui/detailedStats/Stats.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/detailedStats/Stats.java
@@ -5,7 +5,6 @@ import android.app.FragmentTransaction;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.AsyncTask;
-import android.support.v4.os.AsyncTaskCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -14,7 +13,9 @@ import android.view.View;
 import android.widget.Button;
 
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
 
@@ -32,22 +33,45 @@ import se.gu.tux.trux.gui.MainActivity;
 import se.gu.tux.trux.technical_services.NotLoggedInException;
 import tux.gu.se.trux.R;
 
+/**
+ * Möjligtvis snygga till koden genom att:
+ * Skapa en datatyp som innehåller today - total + linegraphseries
+ * Ev låt DataHandler fylla HELA den med data?
+ * Skapa en metod i stats som returnerar dessa mot en key (t ex speed)
+ * OM de är satta, annars null
+ * Låt en inre klass av asynctask fråga regelbundet
+ * Skicka hela datatypen till DetailedStatsFragment.setValues när den är laddad == inte null
+ *
+ *
+ */
 
-public class Stats extends ActionBarActivity {
 
-    Fragment newFragment;
-    Button speedBtn, fuelBtn, distanceBtn, overallBtn;
+public class Stats extends ActionBarActivity implements Serializable {
+
+    DetailedStatsFragment newFragment;
+    Button speedBtn, fuelBtn, distanceBtn;
     FragmentTransaction transaction;
 
-    Speed speedToday;
-    Speed speedWeek;
-    Speed speedMonth;
-    Speed speedTotal;
+    private volatile Speed speedToday;
+    private volatile Speed speedWeek;
+    private volatile Speed speedMonth;
+    private volatile Speed speedTotal;
+    private volatile LineGraphSeries speedValues;
+    private volatile Boolean speedLoaded = false;
 
-    Fuel fuelToday;
-    Fuel fuelWeek;
-    Fuel fuelMonth;
-    Fuel fuelTotal;
+    private volatile Fuel fuelToday;
+    private volatile Fuel fuelWeek;
+    private volatile Fuel fuelMonth;
+    private volatile Fuel fuelTotal;
+    private volatile LineGraphSeries fuelValues;
+    private volatile boolean fuelLoaded = false;
+
+    private volatile Distance distanceToday;
+    private volatile Distance distanceWeek;
+    private volatile Distance distanceMonth;
+    private volatile Distance distanceTotal;
+    private volatile LineGraphSeries distanceValues;
+    private volatile boolean distanceLoaded = false;
 
 
     @Override
@@ -60,23 +84,83 @@ public class Stats extends ActionBarActivity {
         fuelBtn = (Button) findViewById(R.id.fuel_button);
         distanceBtn = (Button) findViewById(R.id.distance_traveled);
 
+        Button.OnClickListener btnOnClick = new ButtonListener();
 
         speedBtn.setOnClickListener(btnOnClick);
         fuelBtn.setOnClickListener(btnOnClick);
         distanceBtn.setOnClickListener(btnOnClick);
 
+        getValues();
     }
-    Button.OnClickListener btnOnClick = new Button.OnClickListener() {
+
+
+
+
+
+     class ButtonListener implements Button.OnClickListener {
+
         @Override
-        public void onClick(View v) {
+        public void onClick(final View v) {
+            newFragment = null;
             if (v == speedBtn) {
                 newFragment = new SpeedWindow();
-            }
-            if (v == fuelBtn) {
+                // Make sure values are set once they are loaded
+                new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        while (!speedLoaded) {
+                            try { Thread.sleep(100); } catch (InterruptedException e) {}
+                        }
+                        System.out.println("WWWWWWWWWWWWWWWWWWWWaiting for ui thread");
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                System.out.println("RRRRRRRRRRRRRRRunnnnnnnnnnnnnnnn");
+                                newFragment.setValues(speedToday, speedWeek, speedMonth, speedTotal,
+                                        speedValues);
+                            }
+                        });
+                        return null;
+                    }
+                }.execute();
+            } else if (v == fuelBtn) {
                 newFragment = new FuelWindow();
-            }
-            if (v == distanceBtn) {
+
+                new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        while (!fuelLoaded) {
+                            try { Thread.sleep(100); } catch (InterruptedException e) {}
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                newFragment.setValues(fuelToday, fuelWeek, fuelMonth, fuelTotal,
+                                        fuelValues);
+                            }
+                        });
+                        return null;
+                    }
+                }.execute();
+            } else if (v == distanceBtn) {
                 newFragment = new DistTravWindow();
+
+                new AsyncTask() {
+                    @Override
+                    protected Object doInBackground(Object[] objects) {
+                        while (!distanceLoaded) {
+                            try { Thread.sleep(100); } catch (InterruptedException e) {}
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                newFragment.setValues(distanceToday, distanceWeek, distanceMonth, distanceTotal,
+                                        distanceValues);
+                            }
+                        });
+                        return null;
+                    }
+                }.execute();
             }
 
             transaction = getFragmentManager().beginTransaction();
@@ -86,6 +170,8 @@ public class Stats extends ActionBarActivity {
             transaction.commit();
         }
     };
+
+
 
     @Override
     public void onBackPressed() {
@@ -124,84 +210,82 @@ public class Stats extends ActionBarActivity {
         startActivity(intent);
     }
     public void getValues() {
-        new AsyncTask() {
+        new Thread(new Runnable() {
             @Override
-            protected Object doInBackground(Object[] objects) {
+            public void run() {
                 try {
+
                     speedToday = (Speed) DataHandler.getInstance().getData(new Speed(MetricData.DAY));
                     speedWeek = (Speed) DataHandler.getInstance().getData(new Speed(MetricData.WEEK));
                     speedMonth = (Speed) DataHandler.getInstance().getData(new Speed(MetricData.THIRTYDAYS));
                     speedTotal = (Speed) DataHandler.getInstance().getData(new Speed(MetricData.FOREVER));
-
-                    final Fuel fuelToday = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.DAY));
-                    final Fuel fuelWeek = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.WEEK));
-                    final Fuel fuelMonth = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.THIRTYDAYS));
-                    final Fuel fuelTotal = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.FOREVER));
 
                     // Create speed object and mark it with current time.
                     // Then request an array of speed average values for last 30 days.
                     Speed mySpeed = new Speed(0);
                     mySpeed.setTimeStamp(System.currentTimeMillis());
                     Data[] avgSpeedPerDay = DataHandler.getInstance().getPerDay(mySpeed, 30);
-                    DataPoint[] speedPoints = new DataPoint[30];
-                    for (int i = 0; i < 30; i++) {
-                        if (avgSpeedPerDay[i].getValue() == null) {
-                            System.out.println("Assuming 0 at null value at pos: " + i);
-                            speedPoints[i] = new DataPoint(i + 1, 0);
-                        } else {
-                            speedPoints[i] = new DataPoint(i + 1, (Double) (avgSpeedPerDay[i]).getValue());
-                        }
-                    }
+                    DataPoint[] speedPoints = getDataPoints(avgSpeedPerDay);
+                    speedValues = new LineGraphSeries(speedPoints);
+
+                    speedLoaded = true;
+                    System.out.println("SPEED LOADED");
+
+
+                    fuelToday = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.DAY));
+                    fuelWeek = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.WEEK));
+                    fuelMonth = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.THIRTYDAYS));
+                    fuelTotal = (Fuel) DataHandler.getInstance().getData(new Fuel(MetricData.FOREVER));
+
+                    Fuel myFuel = new Fuel(0);
+                    myFuel.setTimeStamp(System.currentTimeMillis());
+                    Data[] avgFuelPerDay = DataHandler.getInstance().getPerDay(myFuel, 30);
+                    DataPoint[] fuelPoints = getDataPoints(avgFuelPerDay);
+                    fuelValues = new LineGraphSeries(fuelPoints);
+
+                    fuelLoaded = true;
+                    System.out.println("FUEL LOADED");
+
+
+                    distanceToday = (Distance) DataHandler.getInstance().getData(new Distance(MetricData.DAY));
+                    distanceWeek = (Distance) DataHandler.getInstance().getData(new Distance(MetricData.WEEK));
+                    distanceMonth = (Distance) DataHandler.getInstance().getData(new Distance(MetricData.THIRTYDAYS));
+                    distanceTotal = (Distance) DataHandler.getInstance().getData(new Distance(MetricData.FOREVER));
+
+                    Distance myDistance = new Distance(0);
+                    myDistance.setTimeStamp(System.currentTimeMillis());
+                    Data[] avgDistancePerDay = DataHandler.getInstance().getPerDay(myDistance, 30);
+                    DataPoint[] distancePoints = getDataPoints(avgDistancePerDay);
+                    distanceValues = new LineGraphSeries(distancePoints);
+
+                    distanceLoaded = true;
+                    System.out.println("DISTANCE LOADED");
                 }
                 catch (NotLoggedInException nLIE){
                     System.out.println("NotLoggedInException: " + nLIE.getMessage());
                     Intent intent = new Intent(Stats.this, MainActivity.class);
                     startActivity(intent);
                 }
-
-            return null;
             }
-        }.execute();
-    }
-    public Speed getSpeedToday(){
-      return speedToday;
-    }
-    public Speed getSpeedMonth(){
-        return speedMonth;
-    }
-    public Speed getSpeedWeek(){
-        return speedWeek;
-    }
-    public Speed getSpeedTotal(){
-        return speedTotal;
+        }).start();
     }
 
-    public Fuel getFuelToday(){
-        return fuelToday;
+    public DataPoint[] getDataPoints(Data[] data) {
+
+        DataPoint[] dataPoints = new DataPoint[30];
+        for (int i = 0; i < 30; i++) {
+            if (data[i].getValue() == null) {
+                System.out.println("Assuming 0 at null value at pos: " + i );
+                dataPoints[i] = new DataPoint(i + 1, 0);
+            } else {
+                if(!speedLoaded || !fuelLoaded)
+                    dataPoints[i] = new DataPoint(i + 1, (Double)(data[i]).getValue());
+                else
+                    dataPoints[i] = new DataPoint(i + 1, (Long)(data[i]).getValue());
+            }
+        }
+        return dataPoints;
     }
-    public Fuel getFuelWeek(){
-        return fuelWeek;
-    }
-    public Fuel getFuelMonth(){
-        return fuelMonth;
-    }
-    public Fuel getFuelTotal(){
-        return fuelTotal;
-    }
-/*
-    public Distance getDistanceToday(){
-        return distanceToday;
-    }
-    public Distance getDistanceWeek(){
-        return distanceWeek;
-    }
-    public Distance getDistanceMonth(){
-        return distanceMonth;
-    }
-    public Distance getDistanceTotal(){
-        return distanceTotal;
-    }
-    */
 }
 
 
