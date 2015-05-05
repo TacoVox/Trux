e2b69f19efef01bf1896c729ced83f48232350e3
TacoVox <jonas.kahler@icloud.com>
2015-05-05 12:24:26 +0200

    Added new datatype to get pictures from the database.

---
+++ Trux/app/src/main/java/se/gu/tux/trux/datastructure/Picture.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2015 jonas.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package se.gu.tux.trux.datastructure;
+
+import java.awt.image.BufferedImage;
+
+/**
+ *
+ * @author jonas
+ */
+public class Picture extends Data {
+    BufferedImage img;
+
+    public BufferedImage getImg() {
+        return img;
+    }
+
+    public void setImg(BufferedImage img) {
+        this.img = img;
+    }
+    
+    @Override
+    public Object getValue() {
+        return getImg();
+    }
+
+    @Override
+    public void setValue(Object value) {
+        setImg((BufferedImage) value);
+    }
+
+    @Override
+    public boolean isOnServerSide() {
+        return true;
+    }
+}
