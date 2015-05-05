89d8678ead64c68a92d7dc6dc2a5f4fce84ad540
Niklas le Comte <niklas.lecomte@hotmail.com>
2015-05-05 18:31:04 +0200

    Map is working with finiding current position

--- Trux/app/src/main/java/se/gu/tux/trux/gui/community/FragmentPageAdapterCommunity.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/community/FragmentPageAdapterCommunity.java
@@ -7,45 +7,39 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 
-import se.gu.tux.trux.appplication.DataHandler;
-import se.gu.tux.trux.datastructure.Speed;
-import se.gu.tux.trux.gui.simpleStats.SimpleDistanceTraveledWindow;
-import se.gu.tux.trux.gui.simpleStats.SimpleFuelWindow;
-
-
 public class FragmentPageAdapterCommunity extends FragmentPagerAdapter {
 
     public FragmentPageAdapterCommunity(FragmentManager fm) {
         super(fm);
     }
-
     public Fragment getItem(int arg0) {
 
 
-        try {
-            final Speed speed = (Speed) DataHandler.getInstance().getData(new Speed(0));
-            if (speed != null && (Double) speed.getValue() > 0) {
+        //try {
+        //    final Speed speed = (Speed) DataHandler.getInstance().getData(new Speed(0));
+        //    if (speed != null && (Double) speed.getValue() > 0) {
                 switch (arg0) {
                     case 0:
-                        return new MapFragment();
+                        return new MapFrag();
                     default:
                         break;
                 }
                 return null;
             }
-            else switch (arg0){
-                case 0:
-                    return new MapFragment();
-                default:
-                    break;
-            }
-            return null;
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
+           // else switch (arg0){
 
-    return null;}
+           //     case 0:
+           //         return new MapFrag();
+           //     default:
+           //         break;
+         //   }
+          //  return null;
+       // }
+       // catch (Exception e){
+        //    e.printStackTrace();
+       // }
+
+  //  return null;}
 
     @Override
     public int getCount() {
