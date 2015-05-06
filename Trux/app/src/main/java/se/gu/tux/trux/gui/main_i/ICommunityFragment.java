bb0e12fc60063a200c493de1a1658337982d6ec8
Ivo Vryashkov <ivo.vryashkov@gmail.com>
2015-05-06 14:06:21 +0200

    community fragment fix

--- Trux/app/src/main/java/se/gu/tux/trux/gui/main_i/ICommunityFragment.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/main_i/ICommunityFragment.java
@@ -5,13 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import tux.gu.se.trux.R;
 
 /**
  * Created by ivryashkov on 2015-05-05.
+ *
+ * Handles the community fragment in the main activity.
  */
-public class ICommunityFragment extends Fragment
+public class ICommunityFragment extends Fragment implements View.OnClickListener
 {
 
 
@@ -20,9 +23,25 @@ public class ICommunityFragment extends Fragment
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState)
     {
+        // inflate view
         View view = inflater.inflate(R.layout.fragment_main_i_community, container, false);
 
+        // get components
+        ImageView imageView = (ImageView) view.findViewById(R.id.fragment_main_i_image_view);
+
+        // set listener to components
+        imageView.setOnClickListener(this);
+
+        // return the view
         return view;
+
+    }
+
+
+    @Override
+    public void onClick(View view)
+    {
+        ( (IMainActivity) getActivity() ).onFragmentViewClick(view.getId());
     }
 
 
