3d915da42ef108bddc36496b853a56eed004de51
Niklas le Comte <niklas.lecomte@hotmail.com>
2015-05-11 15:00:56 +0200

    Changed key to the developer consol and also added stuff to mapfrag and also LocationService

--- Trux/app/src/main/java/se/gu/tux/trux/gui/community/MapFrag.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/community/MapFrag.java
@@ -4,6 +4,8 @@ package se.gu.tux.trux.gui.community;
 
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Criteria;
 import android.location.Location;
 
@@ -16,29 +18,36 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import se.gu.tux.trux.application.DataHandler;
+import se.gu.tux.trux.application.LoginService;
 import se.gu.tux.trux.datastructure.Friend;
+import se.gu.tux.trux.datastructure.Picture;
+import se.gu.tux.trux.technical_services.LocationService;
 import se.gu.tux.trux.technical_services.NotLoggedInException;
 import tux.gu.se.trux.R;
 
 public class MapFrag extends Fragment implements OnMapReadyCallback {
 
-    GoogleMap mMap;
-    Friend friend[];
+    private GoogleMap mMap;
+    private Friend friend[];
+    private Picture picture[];
 
-    Timer t;
-    popFriends timer;
-    boolean hasMarker = false;
+    private Timer t;
+    private popFriends timer;
+    private boolean hasMarker = false;
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -76,6 +85,8 @@ public class MapFrag extends Fragment implements OnMapReadyCallback {
         //Gets the satelite pictures as a map
         mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
 
+
+
         //These lines will give you the last known position of the device
         LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
@@ -105,28 +116,45 @@ public class MapFrag extends Fragment implements OnMapReadyCallback {
             try {
                 System.out.println("Inne i popFriends------------------------");
                 friend = DataHandler.getInstance().getFriends();
-                System.out.println("Here is the data from the friend: " + DataHandler.getInstance().getFriends().length);
+                picture = new Picture[friend.length];
+                System.out.println("Here is the length from the friends array: " + DataHandler.getInstance().getFriends().length);
                 if (friend.length > 0) {
                     System.out.println("Inne i friend > 0 ------------------------");
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             for (int i = 0; i < friend.length; i++) {
-                                Friend temp = friend[i];
+                                double temp = friend[i].getUserid();
                                 System.out.println("Here is the friends ID: " + temp);
                                 for(int j = 0; j < friend.length; j++) {
-                                    if (temp.equals(friend[j].getUserid())) {
-                                        System.out.println("It finds the user id and finds the User----------");
-                                        double[] loc = friend[i].getCurrentLoc().getLoc();
-                                        LatLng latLng = new LatLng(loc[0], loc[1]);
-                                        if(hasMarker) {
-                                            mMap.clear();
-                                            hasMarker = false;
+                                    if (temp == friend[j].getUserid()) {
+                                        try {
+                                            picture[j] = DataHandler.getInstance()
+                                                    .getPicture(friend[j].getProfilePic());
+                                            System.out.println("It finds the user id and finds the User----------");
+                                            double[] loc = friend[j].getCurrentLoc().getLoc();
+                                            LatLng latLng = new LatLng(loc[0], loc[1]);
+                                            if(hasMarker) {
+                                                mMap.clear();
+                                                hasMarker = false;
+                                            }
+                                            else
+                                            if(picture != null && picture[j] != null){
+                                                Bitmap bmp;
+                                                BitmapFactory.Options options = new BitmapFactory.Options();
+                                                bmp = BitmapFactory.decodeByteArray(picture[j].getImg(), 0,
+                                                        picture[j].getImg().length, options);
+
+                                                mMap.addMarker(new MarkerOptions().position(latLng).title(
+                                                        "Here is" + friend[j].getFirstname())
+                                                        .icon(BitmapDescriptorFactory.fromBitmap(bmp)));
+                                                hasMarker = true;
+                                            }
                                         }
-                                        else
-                                            mMap.addMarker(new MarkerOptions().position(latLng).title(
-                                                    "Here is" + friend[i].getFirstname()));
-                                            hasMarker = true;
+                                        catch (NotLoggedInException nLIE){
+                                            System.out.println("NotLoggedInException: " + nLIE.getMessage());
+                                        }
+
                                     }
                                 }
                             }
--- Trux/app/src/main/java/se/gu/tux/trux/technical_services/LocationService.java
+++ Trux/app/src/main/java/se/gu/tux/trux/technical_services/LocationService.java
@@ -2,6 +2,7 @@ package se.gu.tux.trux.technical_services;
 
 import android.app.Activity;
 import android.location.Location;
+import android.os.Bundle;
 
 
 import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
@@ -17,7 +18,7 @@ import com.google.android.gms.location.LocationServices;
  */
 public abstract class LocationService implements LocationListener, ConnectionCallbacks, OnConnectionFailedListener {
 
-
+    private Location lastKnownLocation;
     private Location currentLocation;
     private Long locationTimeStamp;
     private double latitude;
@@ -31,6 +32,7 @@ public abstract class LocationService implements LocationListener, ConnectionCal
     public LocationService(Activity activity){
         this.activity = activity;
         buildGoogleApiClient();
+
     }
 
 
@@ -41,6 +43,17 @@ public abstract class LocationService implements LocationListener, ConnectionCal
                 .addOnConnectionFailedListener(this)
                 .addApi(LocationServices.API)
                 .build();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        lastKnownLocation = LocationServices.FusedLocationApi.getLastLocation(
+                googleApiClient);
+        if (lastKnownLocation != null) {
+            latitude = (double) lastKnownLocation.getLatitude();
+            longitude = (double) lastKnownLocation.getLongitude();
+        }
+        startLocationUpdates();
     }
 
     protected void startLocationUpdates() {
@@ -58,22 +71,31 @@ public abstract class LocationService implements LocationListener, ConnectionCal
         locationTimeStamp = System.currentTimeMillis();
     }
 
+    private void updateLocation() {
+        latitude = (double) currentLocation.getLatitude();
+        longitude = (double) currentLocation.getLongitude();
+
+    }
+
     public void setLatitude(double latitude){
         this.latitude = latitude;
     }
+
     public void setLongitude(double longitude){
         this.longitude = longitude;
     }
+
     public double getLatitude(){
         return latitude;
     }
+
     public double getLongitude(){
         return longitude;
     }
+
     public double[] getLatLng(){
         latLng[0] = latitude;
         latLng[1] = longitude;
         return latLng;
     }
-
 }
