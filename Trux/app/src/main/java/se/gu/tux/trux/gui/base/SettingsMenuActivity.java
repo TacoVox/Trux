fa9a580c9042196942370cdc3907e3f9694de770
TacoVox <jonas.kahler@icloud.com>
2015-05-17 12:29:55 +0200

    Changed the file

--- Trux/app/src/main/java/se/gu/tux/trux/gui/base/SettingsMenuActivity.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/base/SettingsMenuActivity.java
@@ -1,14 +1,16 @@
 package se.gu.tux.trux.gui.base;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Switch;
 
 import tux.gu.se.trux.R;
 
-/**
- * Created by ivryashkov on 2015-04-30.
- */
 public class SettingsMenuActivity extends BaseAppActivity
 {
+    private Switch safetySwitch = (Switch) findViewById(R.id.safetySwitch);
+    private Spinner mapTypes = (Spinner) findViewById(R.id.mapSpinner);
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,5 +19,4 @@ public class SettingsMenuActivity extends BaseAppActivity
         setContentView(R.layout.settings_menu_activity);
     }
 
-
-} // end class
+}
