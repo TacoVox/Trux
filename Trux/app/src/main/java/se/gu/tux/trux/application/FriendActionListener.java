0097852b77aff5fb1a2f0acb60c8c5dbf7b3288b
tville <jerker@soundandvision.se>
2015-05-19 14:24:50 +0200

    Put the listener interfaces together

--- Trux/app/src/main/java/se/gu/tux/trux/application/FriendActionListener.java
+++ Trux/app/src/main/java/se/gu/tux/trux/application/FriendActionListener.java
@@ -7,8 +7,11 @@ import se.gu.tux.trux.datastructure.Friend;
 /**
  * Created by jerker on 2015-05-15.
  */
-public interface FriendRequestSentListener {
+public interface FriendActionListener {
+
+    public void onFriendRequestAnswered(long friendId, boolean accepted);
 
     public void onFriendRequestSent(long friendId);
 
+    public void onFriendRemoveSent(long friendId);
 }
