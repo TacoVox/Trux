2f08918241ee640ae20d1789179c996c838257cb
Ivo Vryashkov <ivo.vryashkov@gmail.com>
2015-05-20 14:42:29 +0200

    scaling down bitmap image

--- Trux/app/src/main/java/se/gu/tux/trux/gui/community/CommunityProfileActivity.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/community/CommunityProfileActivity.java
@@ -1,6 +1,7 @@
 package se.gu.tux.trux.gui.community;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -212,8 +213,10 @@ public class CommunityProfileActivity extends BaseAppActivity implements View.On
             {
                 // get the bitmap data
                 bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+
                 // set profile picture in profile page
-                imageView.setImageBitmap(bitmap);
+                imageView.setImageBitmap(decodeSampledBitmapFromResource(getResources(),
+                        bitmap.getGenerationId(), 4096, 4096));
             }
             catch (IOException e)
             {
@@ -222,6 +225,53 @@ public class CommunityProfileActivity extends BaseAppActivity implements View.On
         }
 
     } // end onActivityResult()
+
+
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight)
+    {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth)
+        {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth)
+            {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+
+
+    public static Bitmap decodeSampledBitmapFromResource(Resources res, int resId,
+                                                         int reqWidth, int reqHeight)
+    {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(res, resId, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+        return BitmapFactory.decodeResource(res, resId, options);
+    }
 
 
 
