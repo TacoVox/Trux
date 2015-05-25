8d2aa24e0171378792ddc746ae09c38faf923c52
Ivo Vryashkov <ivo.vryashkov@gmail.com>
2015-05-25 13:37:46 +0200

    option added to send a new message from conversation window

--- Trux/app/src/main/java/se/gu/tux/trux/gui/community/MapCommunityWindow.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/community/MapCommunityWindow.java
@@ -1,25 +1,17 @@
 package se.gu.tux.trux.gui.community;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-import java.util.HashMap;
-
 import se.gu.tux.trux.application.DataHandler;
 import se.gu.tux.trux.datastructure.Friend;
-import se.gu.tux.trux.gui.messaging.FriendListFragment;
 import se.gu.tux.trux.gui.messaging.MessageActivity;
 import tux.gu.se.trux.R;
 
