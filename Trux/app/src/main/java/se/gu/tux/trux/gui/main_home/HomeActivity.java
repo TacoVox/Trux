c15231eb876b8d2593fa49e97a47425d71668dd2
Ivo Vryashkov <ivo.vryashkov@gmail.com>
2015-05-28 14:25:27 +0200

    new background service, keeping notifications alive

--- Trux/app/src/main/java/se/gu/tux/trux/gui/main_home/HomeActivity.java
+++ Trux/app/src/main/java/se/gu/tux/trux/gui/main_home/HomeActivity.java
@@ -21,6 +21,7 @@ import se.gu.tux.trux.gui.base.BaseAppActivity;
 import se.gu.tux.trux.gui.community.CommunityProfileActivity;
 import se.gu.tux.trux.gui.community.FriendsWindow;
 import se.gu.tux.trux.gui.messaging.MessageActivity;
+import se.gu.tux.trux.technical_services.NotificationService;
 import tux.gu.se.trux.R;
 
 
@@ -75,7 +76,12 @@ public class HomeActivity extends BaseAppActivity implements ActionBar.TabListen
         viewPager.setAdapter(pagerAdapter);
 
         // Start timer
-        handler.postDelayed(new StatusRunnable(), 10000);
+        //handler.postDelayed(new StatusRunnable(), 10000);
+
+        Intent intent = new Intent(this, NotificationService.class);
+        intent.setAction("START_NOTIFICATION");
+        startService(intent);
+
     }
 
 
